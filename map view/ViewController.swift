97696907ef4c77905ab1ed74425fafc6be2902e3
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-06-05 13:00:00 +0900

    re

--- map view/ViewController.swift
+++ map view/ViewController.swift
@@ -1,70 +1,79 @@
-//
-//  ViewController.swift
-//  map view
-//
-//  Created by D7703_25 on 2018. 6. 5..
-//  Copyright © 2018년 D7703_25. All rights reserved.
-//
-
 import UIKit
 import MapKit
-class ViewController: UIViewController {
 
-    @IBOutlet weak var myMapview: MKMapView!
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let center = CLLocationCoordinate2D(latitude:35.166016, longitude:129.072564)
         
-        let loc01 = CLLocationCoordinate2D(latitude:35.167818, longitude:129.070586)
+        //번개반점
+        //35.167578, 129.070568
+        //부산광역시 부산진구 양정동 418-282
+        //051-852-9969
         
-         let loc02 = CLLocationCoordinate2D(latitude:35.166078, longitude:129.072522)
+        //DIT
+        //35.165782, 129.072529
+        //부산광역시 부산진구 양정동 양지로 54
+        //051-852-0011
         
-        let span = MKCoordinateSpan(latitudeDelta:0.01,longitudeDelta:0.01)
+        //중심 위도, 경도로 설정
+        let center = CLLocationCoordinate2D(latitude: 35.165782, longitude: 129.072529)
         
+        //번개반점
+        let loc01 = CLLocationCoordinate2D(latitude: 35.167578, longitude: 129.070568)
+        
+        //반경(zoom in, zoom out) 설정
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        //region 객체는 center, span을 묶는것
         let region = MKCoordinateRegion(center: center, span: span)
         
-        myMapview.setRegion(region, animated: true)
+        //지도에 표시하기
+        myMapView.setRegion(region, animated: true)
         
-        
+        ////annotation 표시하기
         let pin01 = MKPointAnnotation()
         pin01.coordinate = loc01
         pin01.title = "번개반점"
         pin01.subtitle = "부산광역시 부산진구 양정동 418-282"
-        myMapview.addAnnotation(pin01)
+        myMapView.addAnnotation(pin01)
         
         let pin02 = MKPointAnnotation()
         pin02.coordinate = center
-        pin02.title = "DIT 동의과학대학교"
-        pin02.subtitle = "부산광역시 부산진구 양정동 429-19"
-        myMapview.addAnnotation(pin02)
+        pin02.title = "동의과학대"
+        pin02.subtitle = "부산광역시 부산진구 양정동 양지로 54"
+        myMapView.addAnnotation(pin02)
         
+        //// geocoding 클로저
         let addr = "부산광역시 부산진구 양정1동 350-1"
-        let geocder = CLGeocoder()
-        geocder.geocodeAddressString(addr){
-        
-            (placemarks: [CLPlacemark]?, error: Error?)-> Void in
-            if let error = error{
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(addr) {
+            
+            (placemarks: [CLPlacemark]?, error: Error?) -> Void in
+            if let error = error {
                 print(error)
                 return
-            }else{
+            } else {
                 print("nil 발생")
             }
-            if let placemarks = placemarks{
-                let placemark = placemarks[0]
+            if let placemarks = placemarks {
                 
-            let loc02 = placemark.location?.coordinate
+                let placemarks = placemarks[0]
+                //                print(placemarks.location!)
+                //                print(placemarks.name!)
+                //                print(placemarks.postalCode!)
+                //                print(placemarks.country!)
+                let loc02 = placemarks.location?.coordinate
                 let pin03 = MKPointAnnotation()
                 pin03.coordinate = loc02!
-                pin03.title = "토마토 도시락"
+                pin03.title = "늘해랑"
                 pin03.subtitle = addr
-                self.myMapview.addAnnotation(pin03)
-            }else{
+                self.myMapView.addAnnotation(pin03)
+            } else {
                 print("nil 발생")
             }
-            
-        
         }
     }
 }
-
